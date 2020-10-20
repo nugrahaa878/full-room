1b152addbcb36d42f04560de194f84ecbb8e6a48
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-10-20 17:10:39 +0700

    [ADD]: sample map

--- fullroom/fullroomapp/views.py
+++ fullroom/fullroomapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+# SAMPLE MAP (For Testing Purposes)
+SAMPLE_MAP = [[0,0,0,1,1], 
+              [0,1,1,0,0], 
+              [0,1,1,1,0], 
+              [0,0,0,0,0],
+              [1,0,0,0,0]]
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'map': SAMPLE_MAP})
