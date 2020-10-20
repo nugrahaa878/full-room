84aa0440f7f0307a075412b8eddbd5d0f2d329d3
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-10-20 17:27:00 +0700

    [ADD]: simple list generator

--- fullroom/fullroomapp/views.py
+++ fullroom/fullroomapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 # SAMPLE MAP (For Testing Purposes)
 SAMPLE_MAP = [[0,0,0,1,1], 
@@ -10,3 +11,7 @@ SAMPLE_MAP = [[0,0,0,1,1],
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'map': SAMPLE_MAP})
+
+# Simple map generator
+def random_map_generator(panjang, lebar):
+    return [[random.randint(0,1) for i in range(panjang)] for j in range(lebar)]
