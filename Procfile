0368dba021fcf38a4aceca9160933e114a381310
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-11-19 13:27:20 +0700

    Fix:  CI/CD

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fullroom.wsgi --log-file -
--- fullroom/settings.py
+++ fullroom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fullroom.urls'
