c3278ffac8fb076f076519691a011b1f5b61c8a4
Ari Nugraha <nugrahaa878@gmail.com>
2020-11-19 19:04:47 +0700

    implement front end

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $(".button-start").click(function () {
+        showStepOne();
+    });
+
+});
+
+function showStepOne() {
+    $("step1-container").hide();
+}
