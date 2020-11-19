321befa88f86a022bb92f0dfe5beb72f5a07de36
Christopher Samuel <samwinar@gmail.com>
2020-11-19 18:50:29 +0700

    Added logic to parse input from webpage and create corresponding map

--- fullroomapp/views.py
+++ fullroomapp/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render
-import random
-
-# SAMPLE MAP (For Testing Purposes)
-SAMPLE_MAP = [[0,0,0,1,1], 
-              [0,1,1,0,0], 
-              [0,1,1,1,0], 
-              [0,0,0,0,0],
-              [1,0,0,0,0]]
 
 # Create your views here.
 def index(request):
-    # Dummy inputs
-    map = base_map_generator(5,5)
-    map = set_invalid_tiles(map, 1, 2)
-    print(map)
-    
-    return render(request, 'index.html', {'map': SAMPLE_MAP})
+    if request.method == 'POST':
+        map = base_map_generator(int(request.POST['x-width']),
+                int(request.POST['y-length']))
 
-# Base map generator
-def base_map_generator(panjang, lebar):
-    return [[1 for i in range(panjang)] for j in range(lebar)]
+        list_of_invalid_tiles = request.POST['invalids'].split(';')
+        for coordinate in list_of_invalid_tiles:
+            coordinate = coordinate.split(',')
+            map = set_invalid_tiles(map, int(coordinate[0]), int(coordinate[1]))
+        # Until parsed later, this shows the resulting map onto the terminal
+        print(map)
+        return render(request, 'index.html', {'map': map})
 
+    return render(request, 'index.html')
+
+# Base map generator, initially all tiles are set to 1 to indicate free tiles
+def base_map_generator(width, length):
+    return [[1 for i in range(width)] for j in range(length)]
+
+# Sets invalid tiles by setting their value to 0
 def set_invalid_tiles(map, x, y):
     map[x][y] = 0
     return map
 
-# CSP algorithm
+# TODO : CSP algorithm, prioritize healthy people first
 def csp():
     pass
