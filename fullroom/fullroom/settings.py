cce2d6a07244c577110ed3be7aa8ae2606dcfdb9
Ari Nugraha <nugrahaa878@gmail.com>
2020-11-19 11:33:21 +0700

    change settings.py (again)

--- fullroom/fullroom/settings.py
+++ fullroom/fullroom/settings.py
@@ -137,7 +137,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
