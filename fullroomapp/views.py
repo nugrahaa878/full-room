e3f03eb47d790aef4fa2ee5d97be343ff3611583
Christopher Samuel <samwinar@gmail.com>
2020-11-19 18:31:30 +0700

    Simplified map generator and added function to set invalid tiles

--- fullroomapp/views.py
+++ fullroomapp/views.py
@@ -10,11 +10,20 @@ SAMPLE_MAP = [[0,0,0,1,1],
 
 # Create your views here.
 def index(request):
+    # Dummy inputs
+    map = base_map_generator(5,5)
+    map = set_invalid_tiles(map, 1, 2)
+    print(map)
+    
     return render(request, 'index.html', {'map': SAMPLE_MAP})
 
-# Simple map generator
-def random_map_generator(panjang, lebar):
-    return [[random.randint(0,1) for i in range(panjang)] for j in range(lebar)]
+# Base map generator
+def base_map_generator(panjang, lebar):
+    return [[1 for i in range(panjang)] for j in range(lebar)]
+
+def set_invalid_tiles(map, x, y):
+    map[x][y] = 0
+    return map
 
 # CSP algorithm
 def csp():
