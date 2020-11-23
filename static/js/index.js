bcfa2799229932683e1885a1c376872cca36e99e
Ari Nugraha <nugrahaa878@gmail.com>
2020-11-23 17:49:50 +0700

    hide step 1 and 2 in the homepage

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,35 @@
 $(document).ready(function () {
 
     $(".button-start").click(function () {
+        hideHomePage();
         showStepOne();
     });
 
 });
 
+function hideStepOne() {
+    $(".panah-1").hide();
+    $(".step1-text").hide();
+    $(".step1-desc").hide();
+    $(".form-1").hide();
+}
+
 function showStepOne() {
-    $("step1-container").hide();
+    $(".panah-1").show();
+    $(".step1-text").show();
+    $(".step1-desc").show();
+    $(".form-1").show();
+}
+
+function hideStepTwo() {
+    $(".panah-2").hide();
+    $(".step2-text").hide();
+    $(".step2-desc").hide();
+    $(".form-2").hide();
+}
+
+function hideHomePage() {
+    $(".description-text").hide();
+    $(".my-button").hide();
+
 }
