5858d94afcfb8c839702297a8666c3a61feff905
Christopher Samuel <samwinar@gmail.com>
2020-11-24 20:30:05 +0700

    Refactored limiter placement into CSP class field, renamed vars and methods

--- fullroomapp/engine.py
+++ fullroomapp/engine.py
@@ -5,10 +5,14 @@ class CSP(object):
 
     board_answer = [[]]
     total_person = 0
+    healthy_people = 0
+    sick_people = 0
     result = False
 
-    def __init__(self, board):
+    def __init__(self, board, healthy_people, sick_people):
         self.board = board
+        self.healthy_people = healthy_people
+        self.sick_people = sick_people
 
     def _print_solution(self):
         for i in range(len(self.board_answer)):
@@ -56,16 +60,16 @@ class CSP(object):
 
         return row, col
 
-    def _move_pointer(self, board, row, col, move_method, counter, people):
+    def _move_pointer(self, board, row, col, move_method, counter):
         """
         Fungsi untuk meng-handle pergerakan pointer sesuai dengan
         move method nya
         """
         if move_method == 0 or move_method == 2:
-            return self.CSP(board, row, col + 1, move_method, counter, people)
+            return self.CSP(board, row, col + 1, move_method, counter)
         
         elif move_method == 1 or move_method == 3:
-            return self.CSP(board, row, col - 1, move_method, counter, people)
+            return self.CSP(board, row, col - 1, move_method, counter)
 
         # return False, counter, main_board
 
@@ -74,14 +78,14 @@ class CSP(object):
             print("".join(board[i]))
 
     # Added graceful stop condition if all people have been positioned
-    def CSP(self, main_board, row, col, move_method, counter, people):
+    def CSP(self, main_board, row, col, move_method, counter):
         length = len(main_board)
 
         # Cek koordinat. Pindahkan pointer ke baris baru dan reset kolom
         row, col = self._check_coor(row, col, move_method)
 
         # Kalau sudah lewat batas, berarti solusi benar
-        if row >= length or row < 0 or counter == people:
+        if row >= length or row < 0 or counter == self.healthy_people:
             return True, counter, main_board
 
         if main_board[row][col] == "_":
@@ -90,7 +94,7 @@ class CSP(object):
                 counter += 1
 
                 temp_result, counter_we_get, main_board = self._move_pointer(
-                    main_board, row, col, move_method, counter, people)
+                    main_board, row, col, move_method, counter)
                 if temp_result == True:
                     return True, counter_we_get, main_board
 
@@ -98,14 +102,14 @@ class CSP(object):
                 main_board[row][col] = "_"
                 counter -= 1
             else:
-                return self._move_pointer(main_board, row, col, move_method, counter, people)
+                return self._move_pointer(main_board, row, col, move_method, counter)
 
         elif main_board[row][col] == "#":
-            return self._move_pointer(main_board, row, col, move_method, counter, people)
+            return self._move_pointer(main_board, row, col, move_method, counter)
 
         return False, counter, main_board
 
-    def run(self, people):
+    def run(self):
         """
         Function untuk menjalankan alforitma CSP
         """
@@ -125,7 +129,7 @@ class CSP(object):
         for method in range(4):
             main_board = deepcopy(self.board)
             row, col = start_point[method]
-            result, total, board_result = self.CSP(main_board, row, col, method, 0, people)
+            result, total, board_result = self.CSP(main_board, row, col, method, 0)
             print(f"total using method {method}: {total}")
 
             if total > max_total and result:
@@ -154,9 +158,9 @@ def solve():
     else:
         csp._print_solution()
 
-def solve_for_healthy(board, people):
-    csp = CSP(board)
-    csp.run(people)
+def solve_for_healthy(board, healthy_people, sick_people):
+    csp = CSP(board, healthy_people, sick_people)
+    csp.run()
 
     if csp.result == False: 
         print("Solution does not exist")
@@ -166,6 +170,5 @@ def solve_for_healthy(board, people):
     
     return csp.board_answer
     
-
 if __name__ == "__main__":
     solve()
--- fullroomapp/views.py
+++ fullroomapp/views.py
@@ -11,39 +11,35 @@ SAMPLE_MAP = [[0,0,0,1,1],
 
 def index(request):
     if request.method == 'POST':
-        map = solve_map_healthy(base_map_generator(int(request.POST['x-width']),
-                int(request.POST['y-length'])), int(request.POST['healthy']))
+        board = solve_board(base_board_generator(int(request.POST['x-width']),
+                int(request.POST['y-length'])), int(request.POST['healthy']),
+                int(request.POST['sick']))
 
-        return render(request, 'index.html', {'map': map})
+        return render(request, 'index.html', {'map': board})
 
     return render(request, 'index.html')
 
-# Base map generator
-def base_map_generator(width, length):
+# Base board generator
+def base_board_generator(width, length):
     return set_invalid_tiles([['_' for i in range(width)] for j in range(length)])
 
 # Sets invalid tiles by setting their value to #
-def set_invalid_tiles(map):
-    for x in range(len(map)):
-        for y in range(len(map[x])):
+def set_invalid_tiles(board):
+    for x in range(len(board)):
+        for y in range(len(board[x])):
             if (random.randint(0, 1) == 1):
-                map[x][y] = '#'
-    return map
+                board[x][y] = '#'
+    return board
 
-# For board/map preview asynchronous calls
-def map_preview(request):
+# For board/board preview asynchronous calls
+def board_preview(request):
     if request.method == 'POST':
-        map = base_map_generator(int(request.POST['x-width']),
+        board = base_board_generator(int(request.POST['x-width']),
                 int(request.POST['y-length']))
-        return map
+        return board
     
     return ["Invalid length and/or width!"]
 
-# For ease of engine method call for solving for healthy people
-def solve_map_healthy(map, people):
-    return fullroomapp.engine.solve_for_healthy(map, people)
-
-# For ease of engine method call for solving for sick people
-def solve_map_sick(map):
-    # Dummy, awaiting sick algorithm
-    return map
+# To solve the board
+def solve_board(board, healthy_people, sick_people):
+    return fullroomapp.engine.solve_for_healthy(board, healthy_people, sick_people)
